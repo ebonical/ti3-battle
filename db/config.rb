95ff56d2273b6b72b5b6a6275ea09ae4f502ae3a
Ebony <ebonical@gmail.com>
2012-10-05 19:39:08 +0100

    remove db from git

--- db/config.rb
+++ db/config.rb
@@ -9,7 +9,7 @@ DataMapper::Property::String.length(255)
 
 # Initialise data map
 # 'postgres://localhost/mydb'
-database_path = ENV['DATABASE_URL'] || 'sqlite://' + File.join(settings.root, "db/#{settings.environment}.db")
+database_path = ENV['DATABASE_URL'] || 'sqlite://' + File.join(settings.root, "db/#{settings.environment}.sqlite")
 DataMapper.setup(:default, database_path)
 
 DataMapper.auto_upgrade!
